36d67436c2d43e0eb204bb93ad35f6c5b89c943c
hxrlz <81855262+hxrlz@users.noreply.github.com>
2021-04-11 11:46:46 +0800

    Create 鸡兔同笼.c

---
+++ 鸡兔同笼.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	float to,tu,t,j;
+	printf("%s\n","总共头数：");
+	scanf("%d\n",&to);
+	printf("%s\n","总共脚数：");
+	scanf("%d\n",&tu);
+	t=tu/2-to;
+	j=to-t;
+	printf("%s%d\n","兔子的数量：",t);
+	printf("%s%d\n","鸡的数量：",j);
+ } 
